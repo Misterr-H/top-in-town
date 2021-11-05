8fbd2a36e38a208d5f4f0bdbaf73f68c88c40eac
Mister-H <55010619+Misterr-H@users.noreply.github.com>
2021-11-05 14:37:31 +0530

    Create keys.js

--- town-server/config/keys.js
+++ town-server/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://misterh:Hs913271@cluster0.nzcav.mongodb.net/topintown?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://<Username>:<Password>@cluster0.nzcav.mongodb.net/topintown?retryWrites=true&w=majority'
 
 module.exports = {
     mongoURI: dbPassword
